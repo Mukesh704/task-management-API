dc3b1fe8116f06c3e5d95b7285590ad81d5889a2
Mukesh704 <150328584+Mukesh704@users.noreply.github.com>
2025-05-16 15:29:05 +0530

    create_new_task controller done

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,4 +1,29 @@
-function createTaskController(req, res) {};
+const taskModel = require('../models/task');
+
+async function createTaskController(req, res) {
+    try {
+        const tokenData = req.user;``
+        const userId = tokenData.id;
+
+        const taskData = req.body;
+
+        const newTask = await taskModel.create({
+            user: userId,
+            ...taskData
+        })
+
+        res.status(200).json({
+            success: true,
+            response: newTask
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            error: 'Internal server error'
+        })
+    }
+};
 
 function getAllUsersTaskController(req, res) {};
 
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { createTaskController, getAllUsersTaskController, getSingleTaskController, updateTaskController, deleteTaskController } = require('../controllers/taskController');
+const { jwtMiddleware } = require('../middlewares/jwtMiddleware');
 
-router.post('/', createTaskController);
+router.post('/', jwtMiddleware, createTaskController);
 router.get('/', getAllUsersTaskController);
 router.get('/:id', getSingleTaskController);
 router.put('/:id', updateTaskController);
